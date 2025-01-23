9314eb6a576b0d1b1ea1e23a10702e414252490e
Pierre M <6030167+devleaks@users.noreply.github.com>
2025-01-23 19:13:59 +0100

    added debug/trace

--- cockpitdecks_wm/resources/weatheravwx.py
+++ cockpitdecks_wm/resources/weatheravwx.py
@@ -17,6 +17,10 @@ logger = logging.getLogger(__name__)
 # logger.setLevel(logging.DEBUG)
 
 
+def nowutc() -> datetime:
+    return datetime.now(timezone.utc)
+
+
 class WeatherAVWX(WeatherData):
 
     def __init__(self, icao: str, taf: bool = False):
@@ -38,6 +42,11 @@ class WeatherAVWX(WeatherData):
         # working variables
         self._raw: str
 
+        # Debugging values
+        self._check_freq = 10  # seconds
+        self._station_check_freq = 60  # seconds
+        self._weather_check_freq = 30  # seconds
+
         self.init()
 
     def init(self):
@@ -48,6 +57,9 @@ class WeatherAVWX(WeatherData):
         else:
             logger.warning(f"invalid or non-existant weather station {self.icao}")
 
+    # ################################################
+    # WeatherData Interface
+    #
     @property
     def label(self):
         return self.station.icao if self.station is not None else "Weather"
@@ -67,13 +79,57 @@ class WeatherAVWX(WeatherData):
     def check_station(self) -> bool:
         """Returns True if station is not defined or is different from weather stattion."""
         if not hasattr(self, "_station") or self._station is None:
+            logger.warning("no station, this should never happen")
             return True
-        return self.station.icao != self.weather.station.icao
+        if self.station.icao != self.weather.station.icao:
+            logger.debug(f"station changed to {self.station.icao}")
+            return True
+        logger.debug("station unchanged")
+        return False
 
     def station_changed(self):
         """Executed when station has changed."""
+        logger.debug("station changed, updating weather")
         if self.update_weather():
             self.weather_changed()
+
+    def weather_changed(self):
+        """Called when weather data has changed"""
+        if self.update_weather():
+            logger.debug("weather updated")
+            super().weather_changed()
+        else:
+            logger.debug("weather unchanged")
+
+    # ################################################
+    # Utility functions
+    #
+    def update_weather(self) -> bool:
+        # 1. Weather data update if weather data is available and station has not changed
+        if hasattr(self, "_weather") and self._weather is not None:
+            if self._weather.station == self.station:  # just need to update metar
+                logger.debug("station not changed")
+                if self._weather.update():
+                    logger.info(f"weather checked, changed: {self._weather.raw}")
+                    self._weather_last_checked = nowutc()
+                    return True
+                logger.info("weather checked, unchanged")
+                return False
+            else:
+                logger.debug("station changed, creating new weather")
+        # 2. New weather data if no weather data or station has changed.
+        logger.debug("new weather..")
+        if self.taf:
+            self._weather = Taf(self.station.icao)
+            self._forecast = []
+        else:
+            self._weather = Metar(self.station.icao)
+        updated = self._weather.update()
+        logger.debug(f"weather created: {updated}")
+        if updated:
+            self._weather_last_checked = nowutc()
+            logger.info(f"weather updated: {self._weather.raw}")
+        return updated
 
     def check_weather(self) -> bool:
         """Check whether weather needs updating.
@@ -83,15 +139,16 @@ class WeatherAVWX(WeatherData):
         - Station has changed
         - Weather data is outdated
         """
-        # 1. No weather data (new)
-        if not hasattr(self, "_weather") or self._weather is None:
+        if not hasattr(self, "_weather") or self._weather is None or self._weather_last_checked is None:
+            logger.debug("no weather")
             return True
-        # 2. Station non existant or changed.
-        if self.check_station():
+        now = nowutc()
+        if (now - self._weather_last_checked).seconds > self._weather_check_freq:
+            logger.debug("weather expired")
             return True
-        # 3. Weather data is outdated
-        diff = datetime.now(timezone.utc) - self.weather.last_updated
-        return diff.seconds > self.update_time
+        else:
+            logger.debug("weather not expired")
+        return False
 
     def has_weather(self):
         # No "raw" attribue, means weather defined but not updated yet
@@ -99,33 +156,6 @@ class WeatherAVWX(WeatherData):
 
     def metar(self) -> str | None:
         return self._weather.raw if hasattr(self.weather, "raw") and not self.taf else None
-
-    def update_weather(self) -> bool:
-        if self.check_weather():
-            # 1. Weather data update if weather data is available and station has not changed
-            if hasattr(self, "_weather") and self._weather is not None:
-                if self._weather.station == self.station:  # just need to update metar
-                    logger.debug("station not changed")
-                    if self._weather.update():
-                        logger.info(f"weather updated: {self._weather.raw}")
-                    return True
-                else:
-                    logger.debug("station changed")
-            # 2. New weather data if no weather data or station has changed.
-            logger.debug("new weather..")
-            if self.taf:
-                self._weather = Taf(self.station.icao)
-                self._forecast = []
-            else:
-                self._weather = Metar(self.station.icao)
-            updated = self._weather.update()
-            logger.debug(f"weather updated: {updated}")
-            if updated:
-                logger.info(f"weather updated: {self._weather.raw}")
-            return updated
-        else:
-            logger.debug("weather does not need updating")
-        return False
 
     def get_forecast_page(self, page: int = 0, width: int = 21) -> List[str]:
         if not self.taf:
